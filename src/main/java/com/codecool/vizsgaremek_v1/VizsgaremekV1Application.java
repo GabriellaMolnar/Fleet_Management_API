5d7df0241e41739e838402ce2ff3eef43d6cfd8d
GabriellaMolnar <molnar.gabriella.gy@gmail.com>
2022-05-16 13:45:21 +0200

    add unversioned files

---
+++ src/main/java/com/codecool/vizsgaremek_v1/VizsgaremekV1Application.java
@@ -0,0 +1,13 @@
+package com.codecool.vizsgaremek_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizsgaremekV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VizsgaremekV1Application.class, args);
+    }
+
+}
