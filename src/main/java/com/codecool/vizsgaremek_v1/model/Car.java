828b9dd9d758d97b10029dce469cb3c6b7d5cbf5
GabriellaMolnar <molnar.gabriella.gy@gmail.com>
2022-05-20 09:38:09 +0200

    add some validation, pom.xml validation

--- src/main/java/com/codecool/vizsgaremek_v1/model/Car.java
+++ src/main/java/com/codecool/vizsgaremek_v1/model/Car.java
@@ -2,10 +2,14 @@ package com.codecool.vizsgaremek_v1.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class Car {
     private int id;
+    @NotBlank
     private String registrationNumber;
+    @NotBlank
     private Brand brand;
     private String model;
     private String color;
--- src/main/java/com/codecool/vizsgaremek_v1/model/Driver.java
+++ src/main/java/com/codecool/vizsgaremek_v1/model/Driver.java
@@ -2,9 +2,15 @@ package com.codecool.vizsgaremek_v1.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class Driver {
+    @NotBlank
+    @Pattern(regexp = "[0-9]")
     private long tribeNumber;
+    @Pattern(regexp = "[^0-9]")
     private String name;
     private String birthDate;
     private String motherName;
