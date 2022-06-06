cf732055ff46f3880d210d134e879f986d22f2ce
GabriellaMolnar <molnar.gabriella.gy@gmail.com>
2022-06-06 14:05:39 +0200

    clean CarAddUpdateDto.java

--- src/main/java/com/codecool/vizsgaremek_v1/entity/dto/CarAddUpdateDto.java
+++ src/main/java/com/codecool/vizsgaremek_v1/entity/dto/CarAddUpdateDto.java
@@ -2,10 +2,8 @@ package com.codecool.vizsgaremek_v1.entity.dto;
 
 import com.codecool.vizsgaremek_v1.entity.Brand;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class CarAddUpdateDto {
 
     private String registrationNumber;
