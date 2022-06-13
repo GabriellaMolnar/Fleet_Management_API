739083abf250f31f5661c75b3c105e5cc271798a
GabriellaMolnar <molnar.gabriella.gy@gmail.com>
2022-06-13 22:28:52 +0200

    add some validation for CarDto and CarValueDto

--- src/main/java/com/codecool/vizsgaremek_v1/entity/dto/CarAddUpdateDto.java
+++ src/main/java/com/codecool/vizsgaremek_v1/entity/dto/CarAddUpdateDto.java
@@ -2,9 +2,11 @@ package com.codecool.vizsgaremek_v1.entity.dto;
 
 import com.codecool.vizsgaremek_v1.entity.Brand;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class CarAddUpdateDto {
@@ -12,8 +14,10 @@ public class CarAddUpdateDto {
     private String registrationNumber;
     @NotNull(message = "brand required")
     private Brand brand;
+    @Length(min = 2, max = 30)
     private String model;
     private String color;
+    @Length(min = 10, max = 40)
     private String engineNumber;
     boolean passengerCar;
 }
