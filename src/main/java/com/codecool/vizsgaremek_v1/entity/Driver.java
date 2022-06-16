55edd49c4c165a0d3f749758b0416d03ff406f43
GabriellaMolnar <molnar.gabriella.gy@gmail.com>
2022-06-16 11:36:08 +0200

    modify Driver entity

--- src/main/java/com/codecool/vizsgaremek_v1/entity/Driver.java
+++ src/main/java/com/codecool/vizsgaremek_v1/entity/Driver.java
@@ -4,14 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
